81be1b73d70af51eaa632c3bc67b8ea28dfd1267
bbmaty <135111007+bbmaty@users.noreply.github.com>
2023-06-12 14:59:06 +0200

    Add files via upload

---
+++ lab01/even1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+	int n = 13;
+
+	if (n % 2 == 0) {
+		printf("%d e' pari\n", n);
+	} else {
+		printf("%d e' dispari\n", n);
+	}
+	return 0;
+}
+
+
---
+++ lab01/numbers3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+	int n = 5;
+	int i = 1;
+
+	do {
+        	printf("%d...", i);
+                i++;
+	} while(i <= n);
+
+        printf("\n");	
+	return 0;
+}
+
+
+
